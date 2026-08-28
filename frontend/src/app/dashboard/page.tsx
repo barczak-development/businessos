adf08e8cd6fb61b84774a23231d3e3da1856d6a9
Ryan B <barczakryan@outlook.com>
2026-08-28 15:44:07 -0400

    added dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div className="display: flex; justify-content: center; align-items: center; height: 100vh;">
+            <h1>Dashboard</h1>
+        </div>
+    );
+}
